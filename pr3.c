bee28b68d186aae85df8c95ee560aeda381ef874
Christopher Dcosta <dcostachristopher211@gmail.com>
2026-08-04 12:01:31 +0530

    Add files via upload

---
+++ pr3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ 	int arr [5],i;
+
+ 	   printf ("enter any 5 number:\n");
+		 
+ 	 	  for (int i=0; i<5 ; i++)
+ 	  {
+ 	    scanf("%d", &arr [i]);
+ 	   }
+		
+		printf("array elements are:\n");
+		
+		 for (int i=0; i<5 ; i++)
+		 {
+		 	printf("%d", arr[i]);
+		 }
+  
+ };
