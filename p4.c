cd304ea811ef51508fca1e228f920e55ffdd7d39
Christopher Dcosta <dcostachristopher211@gmail.com>
2026-08-04 11:48:00 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[10];
+    int i;
+
+    printf("Enter 10 numbers:\n");
+
+    for(i = 0; i < 10; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+
+    for(i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
